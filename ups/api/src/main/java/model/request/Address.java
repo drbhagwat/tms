f112e9b50fd9bb6120ec0579959d4fa4e58c044c
Thamilarasi <vthamilarasi@s3groupinc.com>
2020-05-06 16:58:16 +0530

    Added CRUD operations of company,Divison and warehouse

--- ups/api/src/main/java/model/request/Address.java
+++ ups/api/src/main/java/model/request/Address.java
@@ -6,29 +6,28 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Component
-public class Address {
-  @JsonProperty("AddressLine")
+public class Address implements Serializable {
+  private static final long serialVersionUID = 1L;
+
   private String addressLine;
 
-  @JsonProperty("AddressLine2")
   private String addressLine2;
 
-  @JsonProperty("AddressLine3")
   private String addressLine3;
 
-  @JsonProperty("City")
   private String city;
 
-  @JsonProperty("StateProvinceCode")
   private String stateProvinceCode;
 
-  @JsonProperty("PostalCode")
   private String postalCode;
 
-  @JsonProperty("CountryCode")
   private String countryCode;
 }
