c5a5f9c1c37d8d7986c31d1dca27f33bf7620743
Thamilarasi <vthamilarasi@s3groupinc.com>
2020-05-07 12:01:48 +0530

    Removed unused imports

--- ups/api/src/main/java/com/s3group/tmsapi/core/entities/Warehouse.java
+++ ups/api/src/main/java/com/s3group/tmsapi/core/entities/Warehouse.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import model.request.Address;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EmbeddedId;
