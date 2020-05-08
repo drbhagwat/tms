4d94c4a6e9408661bde4a699f1db8e8fbd8f3cdd
bdinesh <bdinesh@s3groupinc.com>
2020-05-08 20:04:25 +0530

    Wrote proper exception handler...

--- ups/api/src/main/java/com/s3group/tmsapi/TMSApplication.java
+++ ups/api/src/main/java/com/s3group/tmsapi/TMSApplication.java
@@ -2,8 +2,11 @@ package com.s3group.tmsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.s3group.tmsapi.*")
