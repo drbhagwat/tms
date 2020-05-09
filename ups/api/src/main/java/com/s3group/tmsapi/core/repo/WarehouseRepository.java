f2190a3285a607c9403e576f8ef86984e4fc5a73
bdinesh <bdinesh@s3groupinc.com>
2020-05-09 10:14:33 +0530

    Fixed application.yml

--- ups/api/src/main/java/com/s3group/tmsapi/core/repo/WarehouseRepository.java
+++ ups/api/src/main/java/com/s3group/tmsapi/core/repo/WarehouseRepository.java
@@ -1,25 +1,26 @@
-//package com.s3group.tmsapi.core.repo;
-//
-//
-//import com.s3group.tmsapi.core.entities.WarehouseKey;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.repository.PagingAndSortingRepository;
-//import org.springframework.stereotype.Repository;
-//
-///**
-// * This represents repository for Warehouse entity.
-// *
-// * @author : Thamilarasi
-// * @version : 2.0
-// * @since : 2020-05-06
-// */
-//@Repository
-//public interface WarehouseRepository extends PagingAndSortingRepository<Warehouse, WarehouseKey> {
-//    /**
-//     * Finds all warehouses.
-//     *
-//     * @return - all warehouses in the database.
-//     */
-//    Page<Warehouse> findAll(Pageable pageable);
-//}
+package com.s3group.tmsapi.core.repo;
+
+
+import com.s3group.tmsapi.core.entities.Warehouse;
+import com.s3group.tmsapi.core.entities.WarehouseKey;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * This represents repository for Warehouse entity.
+ *
+ * @author : Thamilarasi
+ * @version : 2.0
+ * @since : 2020-05-06
+ */
+@Repository
+public interface WarehouseRepository extends PagingAndSortingRepository<Warehouse, WarehouseKey> {
+  /**
+   * Finds all warehouses.
+   *
+   * @return - all warehouses in the database.
+   */
+  Page<Warehouse> findAll(Pageable pageable);
+}
