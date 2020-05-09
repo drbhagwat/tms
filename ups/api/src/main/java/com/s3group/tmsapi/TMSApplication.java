e42c97b667718354d1fc0c47fa28299f153b1bd8
bdinesh <bdinesh@s3groupinc.com>
2020-05-09 20:18:30 +0530

    chnaged localhost to mic-tms-pg

--- ups/api/src/main/java/com/s3group/tmsapi/TMSApplication.java
+++ ups/api/src/main/java/com/s3group/tmsapi/TMSApplication.java
@@ -2,16 +2,32 @@ package com.s3group.tmsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 @ComponentScan("com.s3group.tmsapi.*")
-@EnableJpaRepositories
+//@EnableEurekaClient
+//@EnableConfigurationProperties({FileStorageProperties.class})
 public class TMSApplication {
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("file:" + System.getenv().get("CONF_DIR") + "/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean getValidator(MessageSource messageSource) {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource);
+		return bean;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TMSApplication.class, args);
 	}
