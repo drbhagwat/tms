381ac2a5dfa7cd9079e541f3b4f331f834f1acff
Anghulakshmi <banghulakshmi@s3groupinc.com>
2020-05-21 16:09:03 +0530

    Did get specific request and response logs with transactionId

--- ups/api/src/main/java/com/s3group/tmsapi/repo/ParcelRequestHistoryRepository.java
+++ ups/api/src/main/java/com/s3group/tmsapi/repo/ParcelRequestHistoryRepository.java
@@ -4,6 +4,9 @@ import com.s3group.tmsapi.entities.request.ParcelRequestHistory;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ParcelRequestHistoryRepository extends PagingAndSortingRepository<ParcelRequestHistory, String> {
+    Optional<ParcelRequestHistory> findByTransactionId(String transactionId);
 }
