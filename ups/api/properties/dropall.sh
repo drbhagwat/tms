6d0dc67531244ccd2e4b6e42bf991f9f70fdd0bf
Meenakshi Sunderesh <msunderesh@s3groupinc.com>
2020-05-29 08:33:34 +0000

    dropall.sh edited online with Bitbucket

--- ups/api/properties/dropall.sh
+++ ups/api/properties/dropall.sh
@@ -1,4 +1,4 @@
 echo "Droping all tables in GIV..."
 export PGPASSWORD=root
-docker cp ./dropall.sql givdeploy_giv-pg_1:tmp/dropall.sql
-docker exec givdeploy_giv-pg_1 psql -U postgres -d postgres -f tmp/dropall.sql
+docker cp ./dropall.sql tms-deploy_mic-tms-pg_1:tmp/dropall.sql
+docker exec tms-deploy_mic-tms-pg_1 psql -U postgres -d postgres -f tmp/dropall.sql
