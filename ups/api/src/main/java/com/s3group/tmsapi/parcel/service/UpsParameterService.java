41579c05a97adaf0280167ef0b9ab278c0c8c35b
Thamilarasi <vthamilarasi@s3groupinc.com>
2020-06-26 18:10:42 +0530

    Did Get Transaction Number API

--- ups/api/src/main/java/com/s3group/tmsapi/parcel/service/UpsParameterService.java
+++ ups/api/src/main/java/com/s3group/tmsapi/parcel/service/UpsParameterService.java
@@ -1,5 +1,7 @@
 package com.s3group.tmsapi.parcel.service;
 
+import com.s3group.tmsapi.common.entities.TransactionId;
+import com.s3group.tmsapi.common.service.TransactionIdService;
 import com.s3group.tmsapi.parcel.entities.UpsParameter;
 import com.s3group.tmsapi.parcel.repo.LabelImageFormatRepository;
 import com.s3group.tmsapi.parcel.repo.LabelStockSizeRepository;
@@ -18,25 +20,30 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class UpsParameterService {
-  @Autowired
-  private ShipmentChargeRepository shipmentChargeRepository;
+    @Autowired
+    private ShipmentChargeRepository shipmentChargeRepository;
 
-  @Autowired
-  private LabelImageFormatRepository labelImageFormatRepository;
+    @Autowired
+    private LabelImageFormatRepository labelImageFormatRepository;
 
-  @Autowired
-  private LabelStockSizeRepository labelStockSizeRepository;
+    @Autowired
+    private LabelStockSizeRepository labelStockSizeRepository;
 
-  @Autowired
-  private UnitOfMeasurementRepository unitOfMeasurementRepository;
+    @Autowired
+    private UnitOfMeasurementRepository unitOfMeasurementRepository;
 
-  public UpsParameter getAllUpsParameters() {
-    UpsParameter upsParameter = new UpsParameter();
-    upsParameter.setShipmentChargeTypes(shipmentChargeRepository.getAllTypes());
-    upsParameter.setImageFormats(labelImageFormatRepository.getAllImageFormatCode());
-    upsParameter.setLabelStockHeightAndWidth(labelStockSizeRepository.getAllHeightAndWidth());
-    upsParameter.setPackageDimensionCodes(unitOfMeasurementRepository.getAllDimensionCode());
+    @Autowired
+    TransactionIdService transactionIdService;
 
-    return upsParameter;
-  }
+    public UpsParameter getAllUpsParameters() {
+        UpsParameter upsParameter = new UpsParameter();
+        upsParameter.setShipmentChargeTypes(shipmentChargeRepository.getAllTypes());
+        upsParameter.setImageFormats(labelImageFormatRepository.getAllImageFormatCode());
+        upsParameter.setLabelStockHeightAndWidth(labelStockSizeRepository.getAllHeightAndWidth());
+        upsParameter.setPackageDimensionCodes(unitOfMeasurementRepository.getAllDimensionCode());
+
+        TransactionId transactionId = transactionIdService.get();
+        upsParameter.setTransactionNumber(transactionId.getTransactionNumber());
+        return upsParameter;
+    }
 }
