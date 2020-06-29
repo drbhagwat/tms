c844e08fa5e774612203633bd1c91e78f9a3748f
bdinesh <bdinesh@s3groupinc.com>
2020-06-29 11:40:59 +0530

    logging added...

--- ups/api/src/main/java/com/s3group/tmsapi/TMSApplication.java
+++ ups/api/src/main/java/com/s3group/tmsapi/TMSApplication.java
@@ -1,36 +1,43 @@
 package com.s3group.tmsapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @SpringBootApplication
 //@EnableEurekaClient
 @ComponentScan("com.s3group.tmsapi.*")
 public class TMSApplication {
-	@Bean
-	public MessageSource messageSource() {
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("file:" + System.getenv().get("CONF_DIR") + "/messages");
-		messageSource.setDefaultEncoding("UTF-8");
-		return messageSource;
-	}
+	private static final Logger LOGGER = LoggerFactory.getLogger(TMSApplication.class);
 
 	@Bean
-	public LocalValidatorFactoryBean getValidator(MessageSource messageSource) {
-		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-		bean.setValidationMessageSource(messageSource);
-		return bean;
-	}
+  public MessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    messageSource.setBasename("file:" + System.getenv().get("CONF_DIR") + "/messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
 
-	public static void main(String[] args) {
-		SpringApplication.run(TMSApplication.class, args);
+  @Bean
+  public LocalValidatorFactoryBean getValidator(MessageSource messageSource) {
+    LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+    bean.setValidationMessageSource(messageSource);
+    return bean;
+  }
+
+  public static void main(String[] args) {
+  	SpringApplication.run(TMSApplication.class, args);
+		LOGGER.debug("This is a debug message");
+		LOGGER.info("This is an info message");
+		LOGGER.warn("This is a warn message");
+		LOGGER.error("This is an error message");
 	}
 }
